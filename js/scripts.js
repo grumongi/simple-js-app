482529db067bf808928f7bc6c5f2f821a8e48a20
grumongi <gustaverumongi@gmail.com>
2024-11-13 14:46:20 -0700

    initial comit 1.2

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemons = [
+    {name: 'Balbusaur' , height: '0.5', type: ['grass', 'poison']},
+    {name:'Squirtle' , height: '0.5,', type: ['water', 'fire'] },
+    {name:'Ivysaur' , height: '1', type: 'flying' }
+];
