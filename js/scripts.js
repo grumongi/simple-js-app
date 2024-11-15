636280ad20472b44ce5301f25bf8059fcea4db78
grumongi <gustaverumongi@gmail.com>
2024-11-15 01:44:40 -0700

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
-let pokemons = [
-    {name: 'Balbusaur' , height: '0.5', type: ['grass', 'poison']},
-    {name:'Squirtle' , height: '0.5,', type: ['water', 'fire'] },
-    {name:'Ivysaur' , height: '1', type: 'flying' }
+let pokemon = [
+    {name: 'Balbusaur', height: '0.5', type: ['grass', 'poison']},
+    {name: 'Squirtle', height: '0.5', type: ['water', 'fire']},
+    {name: 'Ivysaur', height: '1', type: ['flying']}
 ];
+
+for (let i = 0; i < pokemon.length; i++) {
+    let currentPokemon = pokemon[i];
+    console.log('name:', currentPokemon.name);
+    console.log('height:', currentPokemon.height);
+    document.write('<p>' + currentPokemon.name + ' | height: ' + currentPokemon.height + '</p>');
+    if (currentPokemon.height >= 1) {
+        document.write('Wow that is Big!');
+    }
+}
